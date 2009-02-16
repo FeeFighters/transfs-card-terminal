e7e5fad326dc777c0ad4b0a112e8fa28ce8c305a
Joshua Krall <josh@transfs.com>
2009-02-16 03:22:16 -0600

    Fixed capitalization, and adding first cut of ProcessViewController

---
+++ Classes/ProcessViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProcessViewController.h
+//  TransFS Card Terminal
+//
+//  Created by Joshua Krall on 2/16/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ProcessViewController : UIViewController {
+
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TransFS Card Terminal
+//
+//  Created by Joshua Krall on 2/2/09.
+//  Copyright TransFS.com 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
